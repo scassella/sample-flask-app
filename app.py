901d9d914851b3d2a160fd9a4a144705ee81038a
scassella <saracassella@gmail.com>
2018-04-05 12:28:57 -0700

    text fix

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def index():
 def error(errnum=404):
     return "Sorry, there was an error - {}".format(errnum)
 
-app.run(debug=True, port=9000, host='localhost') #debug=True enables hot reload while developing
+app.run(debug=True, port=9000, host='localhost') #debug=True enables reload while developing
